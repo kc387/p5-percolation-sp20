d15df244eef3c0db4ae8aa0f4e621bbb99b263bd
Kathleen Chen <kathleen.chen686@duke.edu>
2020-04-09 09:33:00 -0400

    create PercolationBFS

---
+++ src/PercolationBFS.java
@@ -0,0 +1,15 @@
+public class PercolationBFS extends PercolationDFSFast {
+    /**
+     * Initialize a grid so that all cells are blocked.
+     *
+     * @param n is the size of the simulated (square) grid
+     */
+    public PercolationBFS(int n) {
+        super(n);
+    }
+
+    @Override
+    protected void dfs(int row, int col) {
+        super.dfs(row, col);
+    }
+}
