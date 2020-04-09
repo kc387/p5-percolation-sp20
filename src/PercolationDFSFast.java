669dd8d4e8c8aafa20202edf4a56cb8aaedc9bdd
Kathleen Chen <kathleen.chen686@duke.edu>
2020-04-09 08:59:23 -0400

    updateOnOpen

--- src/PercolationDFSFast.java
+++ src/PercolationDFSFast.java
@@ -1,4 +1,4 @@
-public class PercolationDFSFast extends PercolationDFS {
+public class PercolationDFSFast extends PercolationDFS{
     /**
      * Initialize a grid so that all cells are blocked.
      *
@@ -6,5 +6,18 @@ public class PercolationDFSFast extends PercolationDFS {
      */
     public PercolationDFSFast(int n) {
         super(n);
+    }
+
+    @Override
+    protected void updateOnOpen(int row, int col) {
+        if(row == 0) {
+            dfs(row, col);
+        }
+        else if (isFull(row - 1, col) == true || isFull(row + 1, col) == true){
+            dfs(row, col);
+        }
+        else if (isFull(row, col - 1) == true || isFull(row, col + 1) == true){
+            dfs(row, col);
+        }
     }
 }
