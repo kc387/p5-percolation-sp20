cc7353d212f85a28fb8fa754325f396ef2b1af5b
Kathleen Chen <kathleen.chen686@duke.edu>
2020-04-11 16:32:48 -0400

    test

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -59,7 +59,7 @@ public class PercolationStats {
 	
 	public static void main(String[] args) {
 		PercolationStats ps = new PercolationStats();
-		int trials = 10;
+		int trials = 20;
 		int first = 100;
 		int last = 400; //3200;
 		System.out.printf("simulation data for %d trials\n",trials);
